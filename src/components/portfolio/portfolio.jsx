d5105d46a4cf04d5b8e6e61552213374fde9804c
Sudhanshu-0907 <Sudhanshusingh0907@gmail.com>
2022-05-07 21:13:43 +0530

    final final

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -19,7 +19,7 @@ const portfolio = () => {
           <div className="portfolio_item-img">
             <img src={IMG3} alt="" />
           </div>
-          <h3>This is portfolio item title</h3>
+          <h3>Todolist Backend</h3>
           <div className="portfolio_item-CTA">
             <a href="https://github.com/Sudhanshu-0907/TodoList" className="btn" target="_blank">Github</a>
             <a href="https://still-wildwood-85700.herokuapp.com/" className="btn btn-primary" target="_blank">Live Demo</a>
@@ -30,7 +30,7 @@ const portfolio = () => {
           <div className="portfolio_item-img">
             <img src={IMG1} alt="" />
           </div>
-          <h3>This is portfolio item title</h3>
+          <h3>Todolist React</h3>
           <div className="portfolio_item-CTA">
             <a href="https://github.com/Sudhanshu-0907/Todo-List-using-React-Frontend" className="btn" target="_blank">Github</a>
             <a href="#" className="btn btn-primary" >Live Demo</a>
@@ -41,7 +41,7 @@ const portfolio = () => {
           <div className="portfolio_item-img">
             <img src={IMG2} alt="" />
           </div>
-          <h3>This is portfolio item title</h3>
+          <h3>Keeper App</h3>
           <div className="portfolio_item-CTA">
             <a href="https://github.com/Sudhanshu-0907/Keeper-App" className="btn" target="_blank">Github</a>
             <a href="#" className="btn btn-primary" >Live Demo</a>
@@ -54,7 +54,7 @@ const portfolio = () => {
           <div className="portfolio_item-img">
             <img src={IMG4} alt="" />
           </div>
-          <h3>This is portfolio item title</h3>
+          <h3>Secrete App</h3>
           <div className="portfolio_item-CTA">
             <a href="https://github.com/Sudhanshu-0907/Secret-website" className="btn" target="_blank">Github</a>
             <a href="#" className="btn btn-primary" >Live Demo</a>
@@ -65,7 +65,7 @@ const portfolio = () => {
           <div className="portfolio_item-img">
             <img src={IMG5} alt="" />
           </div>
-          <h3>This is portfolio item title</h3>
+          <h3>Portfolio</h3>
           <div className="portfolio_item-CTA">
             <a href="https://github.com/Sudhanshu-0907/Sudhanshu_portfolio" className="btn" target="_blank">Github</a>
             <a href="https://sudhanshu-0907.github.io/Sudhanshu_portfolio" target="_blank" className="btn btn-primary" >Live Demo</a>
